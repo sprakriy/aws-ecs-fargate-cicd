515760d600665401b44d85ff6efa98ed150468b9
sprakriy <sprakriya@yahoo.com>
2026-01-16 16:56:39 -0500

    Bootstrap complete: Remote state and OIDC active

---
+++ bootstrap.tf
@@ -0,0 +1,22 @@
+# 1. S3 Bucket for State
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "my-fargate-tfstate-${data.aws_caller_identity.current.account_id}"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration { status = "Enabled" }
+}
+
+# 2. DynamoDB for State Locking
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID" 
+    type = "S" 
+    }
+}
+data "aws_caller_identity" "current" {}
