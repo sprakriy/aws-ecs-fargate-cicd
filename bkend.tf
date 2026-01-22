4009ffda8b68e298bc34a1dff75b599788e6fdda
sprakriy <sprakriy@yahoo.com>
2026-01-22 10:43:09 -0500

    changed bucket name

--- bkend.tf
+++ bkend.tf
@@ -2,8 +2,8 @@
 terraform {
   required_version = ">= 1.10.5" # This covers 1.10.x versions  
   backend "s3" {
-    bucket       = "my-fargate-tfstate-319310747432"
-    key          = "terraform.tfstate"
+    bucket       = "sprakriya-tf-state-storage"
+    key          = "fargate-project/terraform.tfstate"
     region       = "us-east-1"
     # dynamodb_table = "terraform-state-locking" #<--- Remove or comment this out
     use_lockfile = true
